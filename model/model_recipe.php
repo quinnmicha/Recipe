cef8d7fcfa4e2655be22c804fb8efc08f0c403d1
quinnmicha <mquinn@email.neit.edu>
2020-03-15 16:21:52 -0400

    Created function to get methods per recipe

--- model/model_recipe.php
+++ model/model_recipe.php
@@ -22,7 +22,30 @@
         }
     }
     
+    //Pulls the info from Recipe Table
+    function getRecipeMethods($recipeId){
+        global $db;
+        
+        $stmt=$db->prepare('SELECT methodOrder AS "Step", methodText AS "Method" FROM Recipe_Method JOIN Recipe ON Recipe_Method.recipeId = Recipe.recipeId WHERE Recipe_Method.recipeId = :recipeId;');
+        
+        $binds=array(
+            ":recipeId"=>$recipeId
+        );
+        
+        
+        if($stmt->execute($binds) && $stmt->rowCount()>0){
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return ($results);
+        }
+        else{
+            return false;
+        }
+    }
+    
     $test = getRecipeIngredients(1);
     var_dump($test);
+    
+    $test1 = getRecipeMethods(1);
+    var_dump($test1);
     
 ?>
