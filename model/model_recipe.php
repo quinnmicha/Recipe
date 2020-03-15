1e06795064193ff6b5268ba48c8b22fce417ab86
quinnmicha <mquinn@email.neit.edu>
2020-03-15 16:18:45 -0400

    Created call to pull ingredients per recipe

--- model/model_recipe.php
+++ model/model_recipe.php
@@ -2,4 +2,27 @@
     include ('db.php');
 
     
+    //Pulls the info from Recipe Table
+    function getRecipeIngredients($recipeId){
+        global $db;
+        
+        $stmt=$db->prepare('SELECT ingredientName AS "Ingredients", ingredientAmt AS "Amount", measurement AS "Type" FROM Recipe_Ingredients JOIN Recipe ON Recipe_Ingredients.recipeId = Recipe.recipeId WHERE Recipe_Ingredients.recipeId = :recipeId');
+        
+        $binds=array(
+            ":recipeId"=>$recipeId
+        );
+        
+        
+        if($stmt->execute($binds) && $stmt->rowCount()>0){
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return ($results);
+        }
+        else{
+            return false;
+        }
+    }
+    
+    $test = getRecipeIngredients(1);
+    var_dump($test);
+    
 ?>
