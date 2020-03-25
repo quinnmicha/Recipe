084c4d34d4ba2ed1669aed813e130adbbce96aec
quinnmicha <mquinn@email.neit.edu>
2020-03-25 09:27:23 -0400

    added validation to login page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if(isPostRequest()){
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<script type="text/javascript" src='../Recipe/model/validation.js'></script>
   
 </head>
 
@@ -53,6 +53,18 @@ if(isPostRequest()){
 <div class="container">
     <form method='POST' class='m-auto w-50'>
         <h1 class='mt-4'>Login</h1>
+        <?php
+            if(isPostRequest())
+            {
+                echo 
+                '<div>
+                    <div class="alert alert-dismissible alert-danger">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong>Login Failed</strong> check your spelling
+                    </div>
+                </div>';
+            } 
+        ?>
         <div class="form-group mt-5">
             <input class='form-control' type="text" name="username" placeholder="Username">
         </div>
