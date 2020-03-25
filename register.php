1e37c06e4d6c5abdb79daa0ad5c5430966971482
quinnmicha <mquinn@email.neit.edu>
2020-03-25 09:17:35 -0400

    began validation for register page

--- register.php
+++ register.php
@@ -23,7 +23,7 @@ if(isPostRequest()){
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<script type="text/javascript" src='../Recipe/model/validation.js'></script>
   
 </head>
 
@@ -50,16 +50,16 @@ if(isPostRequest()){
     <form method='POST' class='m-auto w-50'>
         <h1 class='mt-4'>Register</h1>
         <div class="form-group mt-5">
-            <input class='form-control' type="text" name="username" placeholder="Username">
+            <input class='form-control' type="text" name="username" id='username' placeholder="Username">
         </div>
         <div class="form-group mt-2">
-            <input class='form-control' type="text" name="password" placeholder="Password">
+            <input class='form-control' type="text" name="password" id='password' placeholder="Password">
         </div>
         <div class="form-group mt-2">
-            <input class='form-control' type="text" name="confPassword" placeholder="Confirm Password">
+            <input class='form-control' type="text" name="confPassword" id='confPassword' placeholder="Confirm Password">
         </div>
         <div class="form-group ml-5">
-            <input type='submit' class="btn btn-outline-primary ml-5" value='Submit'>
+            <button type='submit' class="btn btn-outline-primary ml-5" onclick='return checkData()'>Submit</button>
         </div>
     </form>
     
