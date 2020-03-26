d93fc954d0327dd9b49eb37e3b6b259e88d33f38
quinnmicha <mquinn@email.neit.edu>
2020-03-26 14:55:49 -0400

    added comment for myself

--- model/recipeAJAX.php
+++ model/recipeAJAX.php
@@ -5,7 +5,7 @@ include ('model_recipe.php');
 
 //AJAX to add to Recipe table
 
-$userId = input_filer(INPUT_POST, 'userId');
+$userId = input_filer(INPUT_POST, 'userId');//Possibly pull from sessions after login
 $recipeName = input_filter(INPUT_POST, 'recipeName');
 $cookTime = input_filter(INPUT_POST, 'cookTime');
 $recipeImage = input_filter(INPUT_POST, 'recipeImage');
