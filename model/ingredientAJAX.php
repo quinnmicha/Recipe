f0bb24c64c0fb679ab4d31e10b345ecc834e1154
quinnmicha <mquinn@email.neit.edu>
2020-03-30 13:06:09 -0400

    updated php post

--- model/ingredientAJAX.php
+++ model/ingredientAJAX.php
@@ -8,6 +8,6 @@ $ingredientName = filter_input(INPUT_POST, 'ingredientName');
 $ingredientAmt = filter_input(INPUT_POST, 'ingredientAmt');
 $ingredientType = filter_input(INPUT_POST, 'ingredientType');
 
-addIngredient($recipeId['recipeId'], $ingredientName, $ingredientAmt, $ingredientType);
+addIngredient($recipeId[0]['recipeId'], $ingredientName, $ingredientAmt, $ingredientType);
 
 ?>
