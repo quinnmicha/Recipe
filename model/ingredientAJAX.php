2b7e9481456cdb71798b86d4c220395cd052bfc3
quinnmicha <mquinn@email.neit.edu>
2020-03-30 13:03:53 -0400

    found an issue in how I am sending recipeId from function

--- model/ingredientAJAX.php
+++ model/ingredientAJAX.php
@@ -5,9 +5,9 @@ include ('model_recipe.php');
 $recipeId = getRecipeId();
 
 $ingredientName = filter_input(INPUT_POST, 'ingredientName');
-$ingredientAmt = filter_input (INPUT_POST, 'ingredientAmt');
-$ingredientType = filter_input (INPUT_POST, 'ingredientType');
+$ingredientAmt = filter_input(INPUT_POST, 'ingredientAmt');
+$ingredientType = filter_input(INPUT_POST, 'ingredientType');
 
-addIngredient($recipeId, $ingredientName, $ingredientAmt, $ingredientType);
+addIngredient($recipeId['recipeId'], $ingredientName, $ingredientAmt, $ingredientType);
 
 ?>
