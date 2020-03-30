8fb985bc5f3837c52ebf940dbd590822589d3d51
quinnmicha <mquinn@email.neit.edu>
2020-03-30 13:34:37 -0400

    wrote php to send post after AJAX

--- model/methodAJAX.php
+++ model/methodAJAX.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include ('model_recipe.php');
 
+$recipeId = getRecipeId();
+
+$methodOrder = filter_input(INPUT_POST, 'methodOrder');
+$methodText = filter_input(INPUT_POST, 'methodText');
+
+addMethod($recipeId[0]['recipeId'], $methodOrder, $methodText);
+
+?>
