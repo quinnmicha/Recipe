88db8b8c924a317b68bbf3d6ffc9c371f7979c21
quinnmicha <mquinn@email.neit.edu>
2020-03-30 12:43:02 -0400

    setup php call to post AJAX data Untested

--- model/ingredientAJAX.php
+++ model/ingredientAJAX.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$recipeId = getRecipeId();
 
+$ingredientName = filter_input(INPUT_POST, 'ingredientName');
+$ingredientAmt = filter_input (INPUT_POST, 'ingredientAmt');
+$ingredientType = filter_input (INPUT_POST, 'ingredientType');
+
+addIngredient($recipeId, $ingredientName, $ingredientAmt, $ingredientType);
+
+?>
